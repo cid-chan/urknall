c6c6ed57e493bcaf312f360cfe1e9fc58b1e9445
Sarah Netzsch <cid+git@cid-chan.moe>
2022-10-04 11:44:53 +0200

    examples/packer: Don't add a pubkey to terraform.

--- examples/packer/terraform.nix
+++ examples/packer/terraform.nix
@@ -7,9 +7,6 @@
     provisioners.terraform.backend.type = "local";
 
     provisioners.terraform.clouds.hcloud.enable = true;
-    provisioners.terraform.clouds.hcloud.ssh-keys.personal = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx7k8rivHnvsM+AqUhtXXousbwAwGHDFHa3TFrCQgpB";
-    };
     provisioners.terraform.clouds.hcloud.servers.test = {
       type = "cpx11";
       datacenter = "fsn1-dc14";
