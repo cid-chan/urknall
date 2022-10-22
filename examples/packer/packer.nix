a9eb0a9caaf08d582c0e69f0251adf51df8064cc
Sarah Netzsch <cid+git@cid-chan.moe>
2022-10-22 09:57:30 +0200

    examples: fix packer.

--- examples/packer/packer.nix
+++ examples/packer/packer.nix
@@ -29,36 +29,31 @@
           '';
       };
 
-      nixosSystem = (import "${localPkgs.path}/nixos/lib/eval-config.nix" {
-        system = "x86_64-linux";
-        modules = [
-          ({ pkgs, modulesPath, lib, ... }:
-          {
-            imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-            config = {
-              boot.cleanTmpDir = true;
-              zramSwap.enable = true;
-              networking.hostName = lib.mkDefault "test3";
-              services.openssh.enable = true;
-            
-              boot.loader.grub.device = "/dev/sda";
-              boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
-              boot.initrd.kernelModules = [ "nvme" ];
-              users.users.root.openssh.authorizedKeys.keys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx7k8rivHnvsM+AqUhtXXousbwAwGHDFHa3TFrCQgpB"
-              ];
-              fileSystems."/" = {
-                device = "/dev/sda2";
-                fsType = "btrfs";
-              };
-              fileSystems."/boot" = {
-                device = "/dev/sda1";
-                fsType = "ext4";
-              };
+      config = { pkgs, modulesPath, lib, ... }:
+        { 
+          imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+          config = {
+            boot.cleanTmpDir = true;
+            zramSwap.enable = true;
+            networking.hostName = lib.mkDefault "test3";
+            services.openssh.enable = true;
+          
+            boot.loader.grub.device = "/dev/sda";
+            boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+            boot.initrd.kernelModules = [ "nvme" ];
+            users.users.root.openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx7k8rivHnvsM+AqUhtXXousbwAwGHDFHa3TFrCQgpB"
+            ];
+            fileSystems."/" = {
+              device = "/dev/sda2";
+              fsType = "btrfs";
             };
-          })
-        ];
-      });
+            fileSystems."/boot" = {
+              device = "/dev/sda1";
+              fsType = "ext4";
+            };
+          };
+        };
     };
   };
 }
