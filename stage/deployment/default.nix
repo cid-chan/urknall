4abb8d56fcad1f4e58422decf97a6a7713630bc7
Sarah Netzsch <cid+git@cid-chan.moe>
2022-10-29 16:58:06 +0200

    deployment/files: init.
    This allows a user to deploy arbitrary files.

--- stage/deployment/default.nix
+++ stage/deployment/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./nix
     ./nix-v3
+    ./files
   ];
 }
---
+++ stage/deployment/files/default.nix
@@ -0,0 +1,34 @@
+{ localPkgs, config, stages, stage, lib, ... }:
+{
+  options = let inherit (lib) mkOption; inherit (lib.types) attrsOf submodule str; in {
+    deployments.files = mkOption {
+      default = {};
+      type = attrsOf (submodule ({ config, ... }: {
+        options = {
+          host = mkOption {
+            type = str;
+            default = config._module.args.name;
+            description = ''
+              The host to connect to.
+            '';
+          };
+          files = mkOption {
+            type = attrsOf (submodule ./../../_utils/strategies/files/submodule.nix);
+            default = {};
+            description = ''
+              The files to upload to the host.
+            '';
+          };
+        };
+      }));
+    };
+  };
+
+  config = {
+    urknall.appliers = builtins.concatStringsSep "\n" (map (v: ''
+      ${localPkgs.callPackage ./../../_utils/strategies/files/default.nix {
+        module = v.files;
+      }} ${v.host}
+    '') (builtins.attrValues config.deployments.files));
+  };
+}
