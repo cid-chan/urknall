58bddd568c66654b8464ad49a7249e0f87d4c290
Sarah Netzsch <cid+git@cid-chan.moe>
2023-03-25 22:32:35 +0100

    sync: rsync: Ensure trailing slash.

--- stage/state/storages/rsync.nix
+++ stage/state/storages/rsync.nix
@@ -11,13 +11,13 @@
   config = lib.mkIf (config.state.storage.type == "rsync") {
     state.storage.pullCommand = ''
       if [[ -d "${config.state.storage.target}" ]]; then
-        ${localPkgs.rsync}/bin/rsync -a --del "${config.state.storage.target}" "$STATE_CURRENT_DIR"
+        ${localPkgs.rsync}/bin/rsync -a --del "${config.state.storage.target}" "$STATE_CURRENT_DIR/"
       fi
     '';
 
     state.storage.pushCommand = ''
       mkdir -p "${config.state.storage.target}"
-      ${localPkgs.rsync}/bin/rsync -aH --del "$STATE_NEXT_DIR" "${config.state.storage.target}"
+      ${localPkgs.rsync}/bin/rsync -aH --del "$STATE_NEXT_DIR/" "${config.state.storage.target}"
     '';
   };
 }
