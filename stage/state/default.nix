5239cd216b54cb0a431b6b779077ecf4bfebe66c
Sarah Netzsch <cid+git@cid-chan.moe>
2023-03-26 11:20:33 +0200

    state: Add a feature to always update a file.

--- stage/state/default.nix
+++ stage/state/default.nix
@@ -8,45 +8,140 @@ let
 
   prefix = ''
     set -e
-    mkdir -p $STAGE_DIR/{current,next,tmp}
+    mkdir -p $STAGE_DIR/{current,next,tmp,update}
     mkdir -p "${config.state.resultDirectory}"
     STATE_TEMP_DIR="$STAGE_DIR/tmp"
     STATE_CURRENT_DIR="$STAGE_DIR/current"
-    STATE_RESULT_DIR="${config.state.resultDirectory}"
+    STATE_UPDATE_DIR="$STAGE_DIR/update"
     STATE_NEXT_DIR="$STAGE_DIR/next"
+    STATE_RESULT_DIR="${config.state.resultDirectory}"
   '';
 
   commands = ''
+    function copy_maybe_recursive() {
+      FROM="$1"
+      TO="$2"
+      [[ -e "$TO" ]] && rm -r "$TO"
+
+      if [[ -d "$FROM" ]]; then
+        mkdir -p "$TO"
+
+        ${localPkgs.findutils}/bin/find "$FROM" -type f | while read file; do
+          # Decrypt each file to the corresponding target path
+          target_file="$TO''${file#$FROM}"
+          mkdir -p "$(dirname "$target_file")"
+          cp -a "$file" "$target_file"
+        done
+      else
+        cp "$FROM" "$TO"
+      fi
+    }
+
+    # toString False = ""
+    # toString True = "1"
+    function decrypt_recursive_() {
+      copy_maybe_recursive "$1" "$2"
+    }
+    function decrypt_recursive_1() {
+      source_path="$1"
+      target_path="$2"
+      
+      # If the source path is a file, decrypt it to the target path
+      if [[ -f "$source_path" ]]; then
+        mkdir -p "$(dirname "$target_path")"
+        ${decryptor} "$source_path" "$target_path"
+      
+      # If the source path is a directory, decrypt all files within it
+      elif [[ -d "$source_path" ]]; then
+        # Create the target directory if it doesn't exist yet
+        mkdir -p "$target_path"
+        
+        # Loop over all files within the source directory, including subdirectories
+        ${localPkgs.findutils}/bin/find "$source_path" -type f | while read file; do
+          # Decrypt each file to the corresponding target path
+          target_file="$target_path''${file#$source_path}"
+          mkdir -p "$(dirname "$target_file")"
+          ${decryptor} "$file" "$target_file"
+        done
+      fi
+    }
+    function encrypt_recursive_() {
+      copy_maybe_recursive "$1" "$2"
+    }
+    function encrypt_recursive_1() {
+      source_path="$1"
+      target_path="$2"
+      
+      # If the source path is a file, encrypt it to the target path
+      if [[ -f "$source_path" ]]; then
+        mkdir -p "$(dirname "$target_path")"
+        ${encryptor} "$source_path" "$target_path"
+      
+      # If the source path is a directory, encrypt all files within it
+      elif [[ -d "$source_path" ]]; then
+        # Create the target directory if it doesn't exist yet
+        mkdir -p "$target_path"
+        
+        # Loop over all files within the source directory, including subdirectories
+        ${localPkgs.findutils}/bin/find "$source_path" -type f | while read file; do
+          # Decrypt each file to the corresponding target path
+          target_file="$target_path''${file#$source_path}"
+          mkdir -p "$(dirname "$target_file")"
+          ${encryptor} "$file" "$target_file"
+        done
+      fi
+    }
+
+    function compare_files() {
+      if [[ ! -e "$2" ]]; then
+          return 0
+      elif [[ ! -e "$1" ]]; then
+          return 0
+      elif [[ -d "$1" && -f "$2" ]]; then
+          return 0
+      elif [[ -f "$1" && -d "$2" ]]; then
+          return 0
+      elif [[ -f "$1" && -f "$2" ]]; then
+          if ${localPkgs.diffutils}/bin/cmp "$1" "$2"; then
+              return 1
+          else
+              return 0
+          fi
+      elif [[ -d "$1" && -d "$2" ]]; then
+          if ${localPkgs.diffutils}/bin/diff -qr "$1" "$2"; then
+              return 1
+          else
+              return 0
+          fi
+      else
+          echo "Invalid paths provided."
+          return 2
+      fi
+    }
+
+
     ${builtins.concatStringsSep "\n" (lib.mapAttrsToList (name: value: 
       ''
         if [[ -e "$STATE_CURRENT_DIR/${value.generation}-${name}" ]]; then
-          cp "$STATE_CURRENT_DIR/${value.generation}-${name}" "$STATE_NEXT_DIR/${value.generation}-${name}"
+          copy_maybe_recursive "$STATE_CURRENT_DIR/${value.generation}-${name}" "$STATE_NEXT_DIR/${value.generation}-${name}"
 
-          ${if value.sensitive then 
-              ''
-                ${decryptor} "$STATE_CURRENT_DIR/${value.generation}-${name}" "$STATE_RESULT_DIR/${name}"
-              '' 
-            else 
-              ''
-                cp "$STATE_CURRENT_DIR/${value.generation}-${name}" "$STATE_RESULT_DIR/${name}"
-              ''
-          }
+          decrypt_recursive_${toString value.sensitive} "$STATE_CURRENT_DIR/${value.generation}-${name}" "$STATE_TEMP_DIR/${name}"
+
+          ${lib.optionalString value.alwaysUpdate ''
+            ${localPkgs.writeShellScript "generate-${name}" value.generator} "$STATE_UPDATE_DIR/${name}" "$STATE_TEMP_DIR/${name}"
+            if compare_files "$STATE_UPDATE_DIR/${name}" "$STATE_TEMP_DIR/${name}"; then
+              copy_maybe_recursive "$STATE_UPDATE_DIR/${name}" "$STATE_TEMP_DIR/${name}"
+              encrypt_recursive_${toString value.sensitive} "$STATE_UPDATE_DIR/${name}" "$STATE_NEXT_DIR/${value.generation}-${name}"
+              cat "$STATE_NEXT_DIR/${value.generation}-${name}" || true
+            fi
+          ''}
         else
-          ${localPkgs.writeShellScript "generate-${name}" value.generator} "$STATE_TEMP_DIR/${name}"
-
-          ${if value.sensitive then 
-              ''
-                ${encryptor} "$STATE_TEMP_DIR/${name}" "$STATE_NEXT_DIR/${value.generation}-${name}"
-              '' 
-            else 
-              ''
-                cp "$STATE_TEMP_DIR/${name}" "$STATE_NEXT_DIR/${value.generation}-${name}"
-              ''
-          }
-          cp "$STATE_TEMP_DIR/${name}" "$STATE_RESULT_DIR/${name}"
-          rm "$STATE_TEMP_DIR/${name}"
+          ${localPkgs.writeShellScript "generate-${name}" value.generator} "$STATE_TEMP_DIR/${name}" ""
+          encrypt_recursive_${toString value.sensitive} "$STATE_TEMP_DIR/${name}" "$STATE_NEXT_DIR/${value.generation}-${name}"
         fi
 
+        copy_maybe_recursive "$STATE_TEMP_DIR/${name}" "$STATE_RESULT_DIR/${name}"
+        rm -r "$STATE_TEMP_DIR/${name}"
       ''
     ) config.state.files)}
   '';
@@ -153,13 +248,28 @@ in
         options = {
           generator = mkOption {
             type = str;
-            description = "The bash script that is executed to generate this file. $1 will be the path the file should be stored in.";
+            default = "0";
+            description = ''
+              The bash script that is executed to generate this file. 
+
+              Defaults to 0.
+
+              $1 will be the path the file should be stored in.
+
+              If alwaysUpdate is true, $2 will contain the path the current version of the file. If the file has not been created yet, $2 will be empty.
+            '';
           };
 
           sensitive = mkOption {
             type = bool;
             default = false;
             description = "If set to true, the contents of the file will be encrypted.";
+          };
+
+          alwaysUpdate = mkOption {
+            type = bool;
+            default = false;
+            description = "The generator will always run.";
           };
 
           generation = mkOption {
--- stage/state/storages/git.nix
+++ stage/state/storages/git.nix
@@ -4,6 +4,7 @@
   config = lib.mkIf (config.state.storage.type == "git") {
     state.storage.pullCommand = ''
       ${localPkgs.git}/bin/git clone "${config.state.storage.target}" "$STAGE_DIR/repo"
+      sleep 1
       ${localPkgs.rsync}/bin/rsync -a --del --exclude ".git" "$STAGE_DIR/repo/" "$STATE_CURRENT_DIR/"
     '';
 
@@ -13,6 +14,7 @@
       pushd "$STAGE_DIR/repo"
       if [[ ! -z "$(${localPkgs.git}/bin/git status --porcelain)" ]]; then
         ${localPkgs.git}/bin/git add .
+        ${localPkgs.git}/bin/git status
         PATH=$PATH:${lib.makeBinPath [localPkgs.gnupg]} ${localPkgs.git}/bin/git commit -am "State update by $USER ($(${localPkgs.coreutils}/bin/id -u))"
         ${localPkgs.git}/bin/git push "${config.state.storage.target}" "$(${localPkgs.git}/bin/git branch --show-current)" --force
       fi
