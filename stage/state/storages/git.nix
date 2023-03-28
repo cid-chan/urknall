51a1a0573e29cb8ea760c015386b61b027e11824
Sarah Netzsch <cid+git@cid-chan.moe>
2023-03-28 11:20:32 +0200

    state: Allow accessing SSH.

--- stage/state/storages/git.nix
+++ stage/state/storages/git.nix
@@ -3,7 +3,7 @@
 {
   config = lib.mkIf (config.state.storage.type == "git") {
     state.storage.pullCommand = ''
-      ${localPkgs.git}/bin/git clone "${config.state.storage.target}" "$STAGE_DIR/repo"
+      PATH=$PATH:${lib.makeBinPath [localPkgs.openssh]} ${localPkgs.git}/bin/git clone "${config.state.storage.target}" "$STAGE_DIR/repo"
       sleep 1
       ${localPkgs.rsync}/bin/rsync -a --del --exclude ".git" "$STAGE_DIR/repo/" "$STATE_CURRENT_DIR/"
     '';
@@ -16,7 +16,7 @@
         ${localPkgs.git}/bin/git add .
         ${localPkgs.git}/bin/git status
         PATH=$PATH:${lib.makeBinPath [localPkgs.gnupg]} ${localPkgs.git}/bin/git commit -am "State update by $USER ($(${localPkgs.coreutils}/bin/id -u))"
-        ${localPkgs.git}/bin/git push "${config.state.storage.target}" "$(${localPkgs.git}/bin/git branch --show-current)" --force
+        PATH=$PATH:${lib.makeBinPath [localPkgs.openssh]} ${localPkgs.git}/bin/git push "${config.state.storage.target}" "$(${localPkgs.git}/bin/git branch --show-current)" --force
       fi
       popd
     '';
