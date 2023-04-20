15e04811d2ed20e443b30dd24c66fb1c13bbc476
Sarah Netzsch <cid+git@cid-chan.moe>
2023-04-20 14:41:36 +0200

    .ffs

--- stage/_utils/strategies/rescue/submodule.nix
+++ stage/_utils/strategies/rescue/submodule.nix
@@ -61,7 +61,7 @@
             > $out
           chmod +x $out
         '');
-        environment.postBuild = "echo > $out/is_kexec";
+        environment.extraSetup = "echo > $out/is_kexec";
         system.extraDependencies = lib.mkOverride 70 [];
         networking.wireless.enable = lib.mkOverride 500 false;
         hardware.enableRedistributableFirmware = lib.mkOverride 70 false;
