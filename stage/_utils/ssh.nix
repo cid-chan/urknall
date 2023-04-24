f98184462d16c7f9db771c9a80004c522b1e0b44
Sarah Netzsch <cid+git@cid-chan.moe>
2023-04-24 23:31:55 +0200

    wtf???

--- stage/_utils/ssh.nix
+++ stage/_utils/ssh.nix
@@ -1,8 +1,9 @@
-{ writeShellScriptBin, openssh, lib, withExec ? true, nixSsh ? true }:
+{ writeShellScriptBin, openssh, lib, withExec ? true, nixSsh ? true, debug ? false }:
 rec {
   fakeSSH = writeShellScriptBin "ssh" ''
     ${lib.optionalString withExec "exec"} ${lib.optionalString nixSsh "${openssh}/bin/"}ssh \
       -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no \
+      ${lib.optionalString debug "-vvvv"} \
       ''${SSH_KEY:+-i "$SSH_KEY"} \
       "$@"
   '';
@@ -10,6 +11,7 @@ rec {
   fakeSCP = writeShellScriptBin "scp" ''
     ${lib.optionalString withExec "exec"} ${lib.optionalString nixSsh "${openssh}/bin/"}scp \
       -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no \
+      ${lib.optionalString debug "-vvvv"} \
       ''${SSH_KEY:+-i "$SSH_KEY"} \
       "$@"
   '';
