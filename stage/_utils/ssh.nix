337a6bc112a892a40f31c451385bb7d47c04556f
Sarah Netzsch <cid+git@cid-chan.moe>
2024-11-21 10:45:22 +0100

    disable ssh reuse.

--- stage/_utils/ssh.nix
+++ stage/_utils/ssh.nix
@@ -1,20 +1,16 @@
 { writeShellScriptBin, openssh, lib, withExec ? true, nixSsh ? true, debug ? false }:
 rec {
   fakeSSH = writeShellScriptBin "ssh" ''
-    mkdir -p $URKNALL_ROOT_DIR/controlmasters
     ${lib.optionalString withExec "exec"} ${lib.optionalString nixSsh "${openssh}/bin/"}ssh \
       -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no \
-      -oControlPath=$URKNALL_ROOT_DIR/controlmasters/%r@%h:%p -oControlMaster=auto -oControlPersist=yes \
       ${lib.optionalString debug "-vvvv"} \
       ''${SSH_KEY:+-i "$SSH_KEY"} \
       "$@"
   '';
 
   fakeSCP = writeShellScriptBin "scp" ''
-    mkdir -p $URKNALL_ROOT_DIR/controlmasters
     ${lib.optionalString withExec "exec"} ${lib.optionalString nixSsh "${openssh}/bin/"}scp \
       -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no \
-      -oControlPath=$URKNALL_ROOT_DIR/controlmasters/%r@%h:%p -oControlMaster=auto -oControlPersist=yes \
       ${lib.optionalString debug "-vvvv"} \
       ''${SSH_KEY:+-i "$SSH_KEY"} \
       "$@"
