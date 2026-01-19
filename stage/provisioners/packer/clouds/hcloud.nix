1aeb70fd1b2c0a97e3eef3a37f33a0a3cd3817fb
Sarah Netzsch <cid+git@cid-chan.moe>
2026-01-19 15:08:58 +0100

    FUCK HETZNER

--- stage/provisioners/packer/clouds/hcloud.nix
+++ stage/provisioners/packer/clouds/hcloud.nix
@@ -31,7 +31,7 @@ in
 
           serverType = mkOption {
             type = enum ([
-              "cx11" "cpx11" "cx22" "cpx21" "cx32" "cpx31" "cx42" "cpx41" "cx52" "cpx51"              # Shared Resources
+              "cx11" "cpx11" "cx23" "cpx21" "cx32" "cpx31" "cx42" "cpx41" "cx52" "cpx51"              # Shared Resources
               "ccx13" "ccx23" "ccx33" "ccx43" "ccx53" "ccx63"                                         # Dedicated Resources
             ] ++ armTypes);
             default = "cx11";
